ccbea4e5946bd3a035d72b291b12e504fc316017
Qi <zhangq5721@gmail.com>
2025-01-24 19:55:27 +0100

    play with lifecycle

--- day_1/1_effect/q-effect-exercise/src/App.tsx
+++ day_1/1_effect/q-effect-exercise/src/App.tsx
@@ -4,18 +4,20 @@ import Counter from './components/Counter'
 import PostList from './components/PostList'
 import Blog from './components/Blog'
 import Clock from './components/Clock'
+import { Lifecycle } from './components/0_Lifecycle'
 
 function App() {
 
   return (
     <>
-      <Counter></Counter>
+      {/* <Counter></Counter>
       <hr />
       <PostList></PostList>
       <hr />
       <Blog></Blog>
       <hr />
-      <Clock></Clock>
+      <Clock></Clock> */}
+      <Lifecycle></Lifecycle>
     </>
   )
 }
