e43e58e77bc8dc25f8e3ac3c0a2f21fe634763d5
Qi <zhangq5721@gmail.com>
2025-02-02 12:30:54 +0100

    [todo-app] add @transactional to write-operations

--- day_3/app-backend/src/main/java/com/todo/app_backend/services/TodoItemService.java
+++ day_3/app-backend/src/main/java/com/todo/app_backend/services/TodoItemService.java
@@ -10,6 +10,7 @@ import com.todo.app_backend.repositories.TodoListRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -40,9 +41,10 @@ public class TodoItemService {
         return todoItemMapper.toDTO(todoItem);
     }
 
+    @Transactional
     public TodoItemDTO createTodoItem(final Long listId, final TodoItemDTO todoItemDTO) {
         final TodoItem todoItem = todoItemMapper.toEntity(todoItemDTO);
-        if(todoItem.getStatus() == null) {
+        if (todoItem.getStatus() == null) {
             todoItem.setStatus(Status.BACKLOG);
         }
         final TodoList todoList = todoListRepository.findById(listId).orElseThrow();
@@ -51,6 +53,7 @@ public class TodoItemService {
         return todoItemMapper.toDTO(savedTodoItem);
     }
 
+    @Transactional
     public TodoItemDTO updateTodoItemById(final Long listId, final Long itemId, final TodoItemDTO newTodoItemDTO) {
         final TodoItem todoItem = todoItemRepository.findByTodoListIdAndId(listId, itemId).orElseThrow();
         final TodoItem newTodoItem = todoItemMapper.toEntity(newTodoItemDTO);
@@ -59,6 +62,7 @@ public class TodoItemService {
         return todoItemMapper.toDTO(updatedTodoItem);
     }
 
+    @Transactional
     public void deleteTodoItemById(final Long listId, final Long itemId) {
         todoItemRepository.deleteByTodoListIdAndId(listId, itemId);
     }
