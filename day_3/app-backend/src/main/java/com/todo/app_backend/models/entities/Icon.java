ab08cbadacf79953552d675356e93bdeafce35b2
Qi <zhangq5721@gmail.com>
2025-03-25 18:29:47 +0100

    [todo-app] extend icon enum

--- day_3/app-backend/src/main/java/com/todo/app_backend/models/entities/Icon.java
+++ day_3/app-backend/src/main/java/com/todo/app_backend/models/entities/Icon.java
@@ -1,9 +1,14 @@
 package com.todo.app_backend.models.entities;
 
 public enum Icon {
+    ADDALARM("AddAlarm"),
+    BEACHACCESS("BeachAccess"),
     CLOCK("Clock"),
+    FITNESSCENTER("FitnessCenter"),
+    FORMATLISTBULLETED("FormatListBulleted"),
     SHOPPINGCART("ShoppingCart"),
     WORK("Work");
+
 
     public final String label;
 
